ce78d076036458e1e77f913665f6181a620ca87e
Eremin-jpg <byeshyenayaytka@gmail.com>
2024-10-28 17:35:46 +0300

    Добавлена функция нахождения куба

--- code.py
+++ code.py
@@ -2,6 +2,9 @@
 def square(x):
   return x * x
 
+def cube(x):
+  return x * x * x
+
 print("Значение может быть любым действительным числом")
 x = float(input("Введите значение x: "))
 print("Квадрат числа: {0:.5f}".format(square(x)))

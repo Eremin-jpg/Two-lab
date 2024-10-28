a863e83f9045d0e9594bd3f1e10fa4f0855db5cd
Eremin-jpg <byeshyenayaytka@gmail.com>
2024-10-28 17:34:11 +0300

    Добавлена строка описания возможных значений функции

--- code.py
+++ code.py
@@ -2,5 +2,6 @@
 def square(x):
   return x * x
 
+print("Значение может быть любым действительным числом")
 x = float(input("Введите значение x: "))
 print("Квадрат числа: {0:.5f}".format(square(x)))
